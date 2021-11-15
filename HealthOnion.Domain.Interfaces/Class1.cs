9afa4369391bdb98918f47ff161b5ddf5a57a7af
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-15 11:56:46 +0200

    initial onion interface create IPatientrepository

---
+++ HealthOnion.Domain.Interfaces/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HealthOnion.Domain.Core;
+
+namespace HealthOnion.Domain.Interfaces
+{
+    public interface IPatientRepository : IDisposable
+    {
+        IEquatable<Patient> GetPatientList();
+        Patient GetPatient(int id);
+        void Create(Patient patient);
+        void Update(Patient patient);
+        void Delete(int id);
+        void Save();
+    }
+}
