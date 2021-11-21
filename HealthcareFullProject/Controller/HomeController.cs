2b3f282577c2ce0c59aa71421b0bab239c6e2492
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-21 12:34:44 +0200

    add Get list method of patients

--- HealthcareFullProject/Controller/HomeController.cs
+++ HealthcareFullProject/Controller/HomeController.cs
@@ -1,11 +1,10 @@
 ﻿using HealthOnion.Domain.Core;
 using HealthOnion.Domain.Interfaces;
+using HealthOnion.Infrastructure.Data;
 using HealthOnion.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,34 +24,11 @@ namespace HealthcareFullProject.Controller
 
         // GET: api/<HomeController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Patient> Get()
         {
-            return new string[] { "value1", "value2" };
+            return repository.Patients();
         }
 
-        // GET api/<HomeController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<HomeController>
-        //[HttpPost]
-        //public void Post([FromBody] Patient patient)
-        //{
-        //}
-
-        //// PUT api/<HomeController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/<HomeController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+       
     }
 }
