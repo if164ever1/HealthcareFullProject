e74a803f518468c045d7a569eb727696a99becce
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-23 09:54:00 +0200

    add encryption method

---
+++ HealthcareFullProject/Security/CustomHash.cs
@@ -0,0 +1,19 @@
+﻿using EasyEncryption;
+
+namespace HealthcareFullProject.Security
+{
+    public static class CustomHash
+    {
+		internal static string HashPassword(string input)
+		{ 
+			return SHA.ComputeSHA256Hash(input);
+		}
+
+		internal static bool PasswordCheck(string userInput, string savedInDbPassword)
+		{ 
+			string hashedInput = HashPassword(userInput);
+			bool doPasswordsMatch = string.Equals(hashedInput, savedInDbPassword);
+			return doPasswordsMatch;
+		}
+	}
+}
