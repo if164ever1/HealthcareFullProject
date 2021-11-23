76488c3e3d40f708deac79ecaf2ba05f995bed51
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-23 09:46:43 +0200

    add models loginrequest and registerrequest

---
+++ HealthcareFullProject/Models/Request/RegistrationRequest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HealthcareFullProject.Models.Request
+{
+    public class RegistrationRequest
+    {
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
